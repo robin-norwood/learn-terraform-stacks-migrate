7b96f5c8c2841de11ce1af918c15fc7d03d7e166
Robin Norwood <rnorwood@hashicorp.com>
2025-10-29 15:43:16 -0500

    Remove default tags for now

--- main.tf
+++ main.tf
@@ -3,10 +3,6 @@
 
 provider aws {
   region = var.aws_region
-
-  default_tags {
-    tags = var.default_tags
-  }
 }
 
 data "aws_availability_zones" "available" {
--- variables.tf
+++ variables.tf
@@ -4,15 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "default_tags" {
-  description = "Default tags for all resources."
-  type        = map(string)
-  default = {
-    Stack       = "learn-stacks-migrate-aws",
-    Environment = "dev"
-  }
-}
-
 variable "vpc_name" {
   description = "Name of the VPC."
   type        = string
