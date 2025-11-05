67ecc93498b722b884f8ee1bcd5f3e1e28e71bc5
Robin Norwood <rnorwood@hashicorp.com>
2025-11-05 10:07:31 -0600

    Update GCP Terraform block

--- gcp/terraform.tf
+++ gcp/terraform.tf
@@ -18,10 +18,10 @@ terraform {
   }
 
   cloud {
-    organization = "bmcclain"
+    organization = "your-organization-name"
 
     workspaces {
-      name = "learn-terraform-stacks-migrate-robin-gcp"
+      name = "learn-terraform-stacks-migrate"
     }
   }
 
